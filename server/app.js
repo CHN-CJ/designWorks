1ac9527a964dc7fe60c360e278e9e70de6ee9922
CHN-CJ <2873183291@qq.com>
2023-03-15 15:11:53 +0800

    前端搭建

--- server/app.js
+++ server/app.js
@@ -3,6 +3,17 @@ const express = require('express'),
 
 const app = express();
 
+app.use(bodyParser.urlencoded({ extended: true }));
+app.use(bodyParser.json());
+
+app.post('/getUser', (req, res) => {
+    // const { } = req.body;
+    res.send({
+        errorNo: 0,
+        data: 'YEP'
+    });
+})
+
 app.listen('8081', () => {
     console.log('Server is running on PORT 8081');
 })
