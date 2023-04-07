48d20456426a610428421caf506ff2bf687f4250
CHN-CJ <2873183291@qq.com>
2023-04-08 00:49:03 +0800

    Revert "文件上传功能修改"
    This reverts commit 63392e4e32603dce5913a76bae8f0157172830b1.

--- client/designworks-vue/src/main.js
+++ client/designworks-vue/src/main.js
@@ -1,15 +1,5 @@
 import { createApp } from 'vue'
 import App from './App.vue'
 import router from './router'
-import ElementPlus from 'element-plus'
-import 'element-plus/theme-chalk/index.css'
-import * as Icons from '@element-plus/icons-vue'
 
-const app = createApp(App)
-app.use(ElementPlus)
-for (let i in Icons) {
-    app.component(i, Icons[i])
-}
-
-app.use(router).mount('#app')
-
+createApp(App).use(router).mount('#app')
